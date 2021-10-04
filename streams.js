bec5d24fd2bfcef1165da01fae9358f8b45beeb8
Валерий <rasvv@mail.ru>
2021-10-04 22:16:14 +0300

    ДЗ3

--- streams.js
+++ streams.js
@@ -1,56 +1,28 @@
 const fs = require('fs')
-const { Transform } = require('stream')
-// const ACCESS_LOG = 'https://drive.google.com/file/d/1A8B0eDEagkO6XlpJAinsk8_9qQTsnVly/view'
+const readline = require('readline')
+
+
 const ACCESS_LOG = './access.log'
 const firstIP = '89.123.1.41'
 const firstLog = '89.123.1.41_requests.log'
 const secondIP = '34.48.240.111'
 const secondLog = '34.48.240.111_requests.log'
 
-const readStream = fs.createReadStream(require(ACCESS_LOG), {
-	flags: 'r',
-	encoding: 'utf-8',
-})
 
 const firstWriteStream = fs.createWriteStream(firstLog, {flags: 'a', encoding: 'utf-8'})
 const secondWriteStream = fs.createWriteStream(secondLog, {flags: 'a', encoding: 'utf-8'})
 
-let transformedChunk = ''
 
-readStream.on('readable', () => {
-	// console.log('readable');
-	let data
+console.log('Подождите, идет считывание файла');
 
-	while(data = readStream.read()) 
-	if(data.startsWith(secondIP))
-	console.log(data);
-	// chunk.forEach((line) => {
-	// 	console.log(line)
-	// 	if (line.startsWith(secondIP)) {
-	// 		console.log(line)
-	// 	}
-	// })
-	// console.log(cunk)
-	// if (chunk.toString().startsWith(secondIP)) {
-	// 	console.log(chunk.toString())
-	// }
+const rl = readline.createInterface({
+	input: fs.createReadStream(ACCESS_LOG, 'utf-8'),
+	terminal: false
 })
 
+rl.on('line', (line) => {
+	if (line.startsWith(firstIP)) {firstWriteStream.write(`${line}\n`)}
+	else if (line.startsWith(secondIP)) {secondWriteStream.write(`${line}\n`)}
+})
 
-// const transformStream = new Transform({
-// 	transform(chunk, encoding, callback) {
-// 		transformedChunk = chunk.toString()
-// 		if (transformedChunk.startsWith(secondIP)) {
-
-// 			const firstChunk = transformedChunk.replace('34.48.240.111', 'secondIP')
-// 			// console.log('89.123.1.41' + chunk.toString())
-// 			this.push(firstChunk)
-// 		}
-		
-// 		callback()
-// 	}
-// })
-
-// // readStream.pipe(transformStream).pipe(process.stdout)
-// console.log(transformedChunk);
-// readStream.pipe(transformStream).pipe(process.stdout)
+rl.on('error', (error) => console.log(error))
