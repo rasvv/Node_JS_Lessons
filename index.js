ca82e285ec68eb5727776343580599a6248adf66
Валерий <rasvv@mail.ru>
2021-10-14 21:40:34 +0300

    Не понимаю

--- index.js
+++ index.js
@@ -1,70 +1,8 @@
 const fs = require('fs')
-// const { once } = require('events')
-// const inquirer = require('inquirer')
 const path = require("path")
 const readline = require('readline')
 const http = require('http')
-// const { url } = require('inspector')
 
-
-// const dirFiles = document.getElementsByClassName('source')
-
-// let executorDir = ''
-// let list = ''
-
-// const readerLine = async (filePath) => {
-// 	const rl = readline.createInterface({
-// 		input: fs.createReadStream(filePath, 'utf-8'),
-// 		ootput: process.stdout,
-// 	})
-
-// 	rl.on('line', (line) => {
-// 		if (line.includes(searchingString)) {
-// 			++countFinded
-// 		}
-// 	})
-
-// 	rl.on('error', (error) => console.log(error))
-
-// 	await once(rl, 'close')
-// 	console.log(`Количество найденных подстрок '${searchingString}': ${countFinded}`)
-// }
-
-// const lister = () => {
-
-// 	// const listItem = await inquirer
-// 	// .prompt([{
-// 	// 	name: 'fileName',
-// 	// 	type: 'list',
-// 	// 	message: 'Choose file:',
-// 	// 	choices: list,
-// 	// }])
-// 	// .then((answer) => {
-// 	// 	return answer.fileName
-// 	// })
-
-// 	const files = fs.readdirSync(executorDir)
-
-// 	dirFiles.innerHTML = dirFiles
-
-
-// 	// const filePath = path.join(executorDir, listItem)
-
-// 	// if (fs.lstatSync(filePath).isDirectory()) {
-// 	// 	executorDir = filePath
-// 	// 	list = fs.readdirSync(executorDir)
-// 	// 	return await lister()
-// 	// } else {
-// 	// 	readerLine(filePath)			
-// 	// } 
-// }
-
-// const getSource = () => {
-// 	console.log('Select')
-// 	executorDir = process.cwd()
-// 	list = fs.readdirSync(executorDir)
-// 	lister()
-// }
 
 console.log('Start')
 
@@ -74,22 +12,30 @@ let prevListItems = []
 let HTML = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8')
 
 const run = async () => {
-	// const isFile = (filePath) => fs.lstatSync(filePath).isFile()
 	const isFile = (filePath) => fs.lstatSync(filePath).isFile();
 	// const isDir =  (filePath) => fs.lstatSync(filePath).isDirectory()
 
 	http.createServer((req, res) => {
+		console.log('0,1 ' + process.cwd());
+		console.log('0,2 ' + req.url);
+
 		const fullPath = path.join(process.cwd(), req.url)
+		console.log('1 ' + fullPath);
 
 		if (isFile(fullPath)) {
 			
-			HTML.replace('##links', prevListItems)
-			console.log(fs.readFileSync(fullPath), 'utf-8');
-			HTML.replace('##file', fs.readFileSync(fullPath), 'utf-8')
+			// HTML.replace('##links', prevListItems)
+			console.log('2 ' + fs.readFileSync(fullPath).toString())
+			HTML.replace('##file', fs.readFileSync(fullPath).toString())
+			console.log('2,5 ' + HTML)
+			
 		} else {
 			let listItems = ''
 
+			console.log('2,6 ' + req.url);
+			// console.log('2,7 ' + urlParams);
 			const urlParams = req.url.match(/[\d\w\.]+/gi)
+			console.log('3 ' + urlParams);
 
 			if (urlParams) {
 				urlParams.pop()
@@ -98,25 +44,25 @@ const run = async () => {
 				listItems = urlParams.length ? `<li><a href="/${prevUrl}">...</a></li>` : `<li><a href="/">...</a></li>`;
 
 			}
+			console.log('3,5 ' + listItems);
 
 			let list = fs.readdirSync(fullPath)
+			console.log('4 ' + list);
 
 			if (list.length) {
 				list.forEach((listItem) => {
 					const filePath = path.join(req.url, listItem)
-					listItems += `<li><a class='sourceItem' href='${filePath}'>${listItem}</a></li>`
+					listItems += `<li><a href='${filePath}'>${listItem}</a></li>`
 				})
 			}
+
+			prevListItems = listItems
+			console.log('5 ' + prevListItems);
 
 			HTML = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8')
 								.replace('##links', listItems)
 
 		} 
-
-
-							
-		// const sourceDir =  HTML.getElementsByClassName('source')
-		// sourceDir[0].innerHTML = listItems
 
 		res.writeHead(200, {
 			'Content-Type': 'text/html',
